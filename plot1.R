11294b8e767d9696806547692f0ad0cb36b9cf81
Vadivelpl <vadivelpl@live.com>
2015-09-14 00:00:36 +0530

    assignment_1

---
+++ plot1.R
@@ -0,0 +1,10 @@
+inp1 <- read.table("C:/Vadivel/Work/Analytics/coursera - Exploratory data analytics/Assignment/household_power_consumption.txt",header=TRUE,sep=";")
+inp <- inp1 #taking backup of the read data
+inp$Date <- as.Date(inp$Date,"%d/%m/%Y")
+
+inp$Global_active_power <- as.numeric(inp$Global_active_power)
+inp$Global_active_power <- inp$Global_active_power/1000 #converting watts to kilowatts
+
+#filtering for 2 days in feb 2007
+febdata <- subset(inp, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+hist(febdata$Global_active_power)
